e672867b38ebfb034884be2f20b67765db8a1821
iljah <ilja200303@gmail.com>
2022-12-27 21:58:03 +0200

    'play demo' button was added, ai photo was changed from png to svg and some other bugs were fixed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./header.css";
 import ai from "../../assets/ai.png"
+import ai_2 from "../../assets/ai_2.svg"
 
 function Header(props) {
     return (
@@ -26,35 +27,27 @@ function Header(props) {
 
                 </div>
 
-                {/*<button>*/}
-                {/*    <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
-                {/*        <path d="M15.4629 8.31237L0.0222387 16.2365L0.0222387 0.388224L15.4629 8.31237Z" fill="white"/>*/}
-                {/*    </svg>*/}
-                {/*    play demo*/}
-                {/*</button>*/}
+                <div className="btn">
+                    <a href="#play_demo">
+                        <span className="triangle">
+                            <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15.4629 8.31237L0.0222387 16.2365L0.0222387 0.388224L15.4629 8.31237Z" fill="white"/>
+                            </svg>
 
-                {/*<div className="on-light">*/}
-                {/*    <button className="border-gradient border-gradient-purple">*/}
-                {/*        <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
-                {/*            <path d="M15.4629 8.31237L0.0222387 16.2365L0.0222387 0.388224L15.4629 8.31237Z" fill="white"/>*/}
-                {/*        </svg>*/}
-                {/*        play demo*/}
-                {/*    </button>*/}
-                {/*</div>*/}
+                        </span>
 
-                <a className="btn" href="#">
-                    <span>
-                        <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M15.4629 8.31237L0.0222387 16.2365L0.0222387 0.388224L15.4629 8.31237Z" fill="white"/>
-                        </svg>
-                        play demo
-                    </span>
-                </a>
+                        <span className="play-demo">
+                            play demo
+                        </span>
+                    </a>
+
+                </div>
             </div>
 
             <div className="header-image">
-                <img src={ai} alt="ai"/>
+                <img src={ai_2} alt="ai"/>
             </div>
+
 
         </div>
     );
